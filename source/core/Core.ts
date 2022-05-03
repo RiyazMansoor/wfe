49377b352fc3c94a8cc58564d777f2ba8aab2b6b
Riyaz Mansoor <riyaz.mansoor@gmail.com>
2022-05-03 22:20:47 +0500

    setting up

--- source/core/Core.ts
+++ source/core/Core.ts
@@ -1,4 +1,32 @@
 
+
+type NID = string ;
+type Email = string ;
+type DInstanceId = string ;
+
+type DType = string | number ;
+
+type DBlock = Map<string, DType|DBlock> ;
+
+type Workflow = {
+    wfType: string,
+    wfInstanceId: DInstanceId,
+    wfCreatedAt: number,
+    wfCreatedBy: string,
+    wfClosedAt: number,
+    wfClosedBy: string,
+    wfNodes: DInstanceId[]
+}
+
+type ParentWorkFlow = {
+    pwfType: string,
+    pwfInstanceId: DInstanceId,
+    pwfNodeFromType: string,
+    pwfNodeFromInstanceId: DInstanceId,
+    pwfNodeToType: string,
+    pwfNodeToInstanceId: DInstanceId,
+}
+
 abstract class Workflow {
 
     protected wfType: string = null ;
