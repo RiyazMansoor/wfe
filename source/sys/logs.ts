82884f20b195deaf7efb7bb4e6d0b093834a493e
rmanchu <riyaz.mansoor@gmail.com>
2022-10-04 12:46:01 +0500

    updates

--- source/sys/logs.ts
+++ source/sys/logs.ts
@@ -40,6 +40,41 @@ type T_Log = {
     details: string
 }
 
+import * as fb from "firebase/firestore";
+
+class Post {
+    constructor(readonly title: string, readonly author: string) {}
+  
+    toString(): string {
+      return this.title + ', by ' + this.author;
+    }
+  }
+  
+  const postConverter = {
+    toFirestore(post: WithFieldValue<Post>): DocumentData {
+      return {title: post.title, author: post.author};
+    },
+    fromFirestore(
+      snapshot: QueryDocumentSnapshot,
+      options: SnapshotOptions
+    ): Post {
+      const data = snapshot.data(options)!;
+      return new Post(data.title, data.author);
+    }
+  };
+  
+  const postSnap = await firebase.firestore()
+    .collection('posts')
+    .withConverter(postConverter)
+    .doc().get();
+  const post = postSnap.data();
+  if (post !== undefined) {
+    post.title; // string
+    post.toString(); // Should be defined
+    post.someNonExistentProperty; // TS error
+  }
+
+  
 /**
  * This interface is for sys admins - to review/trace issues. 
  * It is expected that finer searching, filtering will happen client side.
